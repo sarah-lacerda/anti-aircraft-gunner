3042a1559fb75117c9e984c74527240894932fcf
Sarah Lacerda <sarah.lacerda@mail.com>
2021-05-23 22:42:57 -0300

    Move window instance to class level

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,14 +2,15 @@
 import static de.damios.guacamole.gdx.StartOnFirstThreadHelper.startNewJvmIfRequired;
 
 public class Application {
+
+    private static final Window WINDOW = Window.getInstance();
+
     public static void main(String[] args) {
         // Starts a new JVM if the application was started on macOS without the
         // -XstartOnFirstThread argument.
         if (startNewJvmIfRequired()) {
             System.exit(0);
         }
-
-        Window window = Window.getInstance();
-        window.run();
+        WINDOW.run();
     }
 }
